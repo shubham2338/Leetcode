dd8b5e3d01f6fab8cc8195a83a50488a9f769684
Shubham Singh <77970687+shubham2338@users.noreply.github.com>
2023-03-21 23:01:06 +0530

    Time: 922 ms (48.39%), Space: 23 MB (58.06%) - LeetHub

--- 2348-number-of-zero-filled-subarrays/2348-number-of-zero-filled-subarrays.py
+++ 2348-number-of-zero-filled-subarrays/2348-number-of-zero-filled-subarrays.py
@@ -1,13 +1,13 @@
-import math
 class Solution(object):
     def zeroFilledSubarray(self, nums):
         ans=0
         i=0
-        j=0
-        while i<len(nums):
-            if nums[i]!=0:
-                j=i+1
-            ans+=i-j+1
+        while i <len(nums):
+            c=0
+            while i<len(nums) and nums[i]==0:
+                c+=1
+                ans+=c
+                i+=1
             i+=1
         return ans
         
