bffbec6f72abb623ca4cffe7ff35befc87098b73
Shubham Singh <77970687+shubham2338@users.noreply.github.com>
2023-04-04 10:48:39 +0530

    Time: 264 ms (20.48%), Space: 71.9 MB (21.72%) - LeetHub

--- 2405-optimal-partition-of-string/2405-optimal-partition-of-string.cpp
+++ 2405-optimal-partition-of-string/2405-optimal-partition-of-string.cpp
@@ -1,18 +1,15 @@
-class Solution:
-    def partitionString(self, s: str) -> int:
-        res=1
-        j=0
-        r=''
-        while j<len(s):
-            if s[j] in r:
-                r=''
-                res+=1
-                j-=1
-            else:
-                r+=s[j]
-            j+=1
-        return res
-        
-                
-            
-                
+class Solution {
+public:
+    int partitionString(string s) {
+        int i=0,res=1;
+        set<char>h;
+        for(i=0;i<s.length();i++){
+            if(h.find(s[i])!=h.end()){
+                res++;
+                h.clear();
+            }
+            h.insert(s[i]);
+        }
+        return res;
+    }
+};
