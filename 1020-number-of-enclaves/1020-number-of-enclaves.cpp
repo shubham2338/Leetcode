541e43ac369c3ea13732d53f0fea72ca2dbbd0e7
Shubham Singh <77970687+shubham2338@users.noreply.github.com>
2023-04-07 09:46:21 +0530

    Time: 70 ms (88.73%), Space: 28.3 MB (68.59%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.cpp
+++ 1020-number-of-enclaves/1020-number-of-enclaves.cpp
@@ -1,25 +1,28 @@
 class Solution {
 public:
-    int numEnclaves(vector<vector<int>>& A) {
-        int all_one=0;
-        for(int i=0;i<A.size();i++){
-            for(int j=0;j<A[0].size();j++)
-                    all_one+=A[i][j];
+    int numEnclaves(vector<vector<int>>& grid) {
+        int res=0;
+        int res1=0;
+        for(int i=0;i<grid.size();i++){
+            for(int j=0;j<grid[0].size();j++)
+                    res1+=grid[i][j];
         }
         int close_one = 0;
-        for(int i=0;i<A.size();i++){
-            for(int j=0;j<A[0].size();j++){
-                if((i==0 || j==0 || i==A.size()-1 || j==A[0].size()-1) && A[i][j]==1)
-                    close_one+= dfs(i,j,A);
+        for(int i=0;i<grid.size();i++){
+            for(int j=0;j<grid[0].size();j++){
+                if((i==0 || j==0 || i==grid.size()-1 || j==grid[0].size()-1) && grid[i][j]==1)
+                    res+= dfs(i,j,grid);
             }
         }
-        return all_one - close_one;
+        return res1 - res;
     }
                    
     int dfs(int i,int j,vector<vector<int>> &grid){
         if( i<0 || j<0 || i>=grid.size() || j>=grid[0].size() || grid[i][j]==0)
             return 0;
+        
         grid[i][j] = 0;
+        
         return 1+dfs(i-1,j,grid)+dfs(i+1,j,grid)+dfs(i,j-1,grid)+dfs(i,j+1,grid);
     }
 };
